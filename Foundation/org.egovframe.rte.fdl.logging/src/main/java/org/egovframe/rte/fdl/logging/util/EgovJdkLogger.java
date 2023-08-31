4d085a6775286a8085702b7aedeb36b7f79a6229
김태현 사원 <thyun7025@lgcns.com>
2023-08-31 18:41:57 +0900

    java.util.logging.Level에 따른 log level 추가

--- Foundation/org.egovframe.rte.fdl.logging/src/main/java/org/egovframe/rte/fdl/logging/util/EgovJdkLogger.java
+++ Foundation/org.egovframe.rte.fdl.logging/src/main/java/org/egovframe/rte/fdl/logging/util/EgovJdkLogger.java
@@ -34,13 +34,25 @@ import java.util.logging.Logger;
  */
 public final class EgovJdkLogger {
 
-	private static final Level IGNORE_INFO_LEVEL = Level.OFF;
+	private static final Level ALL_INFO_LEVEL = Level.ALL;
+	private static final Level CONFIG_INFO_LEVEL = Level.CONFIG;
+	private static final Level FINE_INFO_LEVEL = Level.FINE;
+	private static final Level FINER_INFO_LEVEL = Level.FINER;
 	private static final Level DEBUG_INFO_LEVEL = Level.FINEST;
 	private static final Level INFO_INFO_LEVEL = Level.INFO;
+	private static final Level IGNORE_INFO_LEVEL = Level.OFF;
+	private static final Level SEVERE_INFO_LEVEL = Level.SEVERE;
+	private static final Level WARNING_INFO_LEVEL = Level.WARNING;
 
-	private static final Logger IGNORE_LOGGER = Logger.getLogger("ignore");
+	private static final Logger ALL_LOGGER = Logger.getLogger("all");
+	private static final Logger CONFIG_LOGGER = Logger.getLogger("config");
+	private static final Logger FINE_LOGGER = Logger.getLogger("fine");
+	private static final Logger FINER_LOGGER = Logger.getLogger("finer");
 	private static final Logger DEBUG_LOGGER = Logger.getLogger("debug");
 	private static final Logger INFO_LOGGER = Logger.getLogger("info");
+	private static final Logger IGNORE_LOGGER = Logger.getLogger("ignore");
+	private static final Logger SEVERE_LOGGER = Logger.getLogger("severe");
+	private static final Logger WARNING_LOGGER = Logger.getLogger("warning");
 	
 	private EgovJdkLogger() {
 	}
@@ -82,10 +94,129 @@ public final class EgovJdkLogger {
 	}
 
 	/**
-	 * 일반적이 정보를 기록하는 경우 사용.
+	 * 일반적인 정보를 기록하는 경우 사용.
+	 */
+	public static void info(String message, Exception exception) {
+		if (exception == null) {
+			INFO_LOGGER.log(INFO_INFO_LEVEL, message);
+		} else {
+			INFO_LOGGER.log(INFO_INFO_LEVEL, message, exception);
+		}
+	}
+
+	/**
+	 * 일반적인 정보를 기록하는 경우 사용.
 	 */
 	public static void info(String message) {
 		INFO_LOGGER.log(INFO_INFO_LEVEL, message);
 	}
 
+	/**
+	 * 모든 정보를 기록하는 경우 사용.
+	 */
+	public static void all(String message, Exception exception) {
+		if (exception == null) {
+			ALL_LOGGER.log(ALL_INFO_LEVEL, message);
+		} else {
+			ALL_LOGGER.log(ALL_INFO_LEVEL, message, exception);
+		}
+	}
+
+	/**
+	 * 모든 정보를 기록하는 경우 사용.
+	 */
+	public static void all(String message) {
+		ALL_LOGGER.log(ALL_INFO_LEVEL, message);
+	}
+
+
+	/**
+	 * 정적인 구성 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void config(String message, Exception exception) {
+		if (exception == null) {
+			CONFIG_LOGGER.log(CONFIG_INFO_LEVEL, message);
+		} else {
+			CONFIG_LOGGER.log(CONFIG_INFO_LEVEL, message, exception);
+		}
+	}
+
+	/**
+	 * 정적인 구성 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void config(String message) {
+		CONFIG_LOGGER.log(CONFIG_INFO_LEVEL, message);
+	}
+
+	/**
+	 * 트레이스 정보 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void fine(String message, Exception exception) {
+		if (exception == null) {
+			FINE_LOGGER.log(FINE_INFO_LEVEL, message);
+		} else {
+			FINE_LOGGER.log(FINE_INFO_LEVEL, message, exception);
+		}
+	}
+
+	/**
+	 * 트레이스 정보 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void fine(String message) {
+		FINE_LOGGER.log(FINE_INFO_LEVEL, message);
+	}
+
+	/**
+	 * 상세한 트레이스 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void finer(String message, Exception exception) {
+		if (exception == null) {
+			FINER_LOGGER.log(FINER_INFO_LEVEL, message);
+		} else {
+			FINER_LOGGER.log(FINER_INFO_LEVEL, message, exception);
+		}
+	}
+
+	/**
+	 * 상세한 트레이스 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void finer(String message) {
+		FINER_LOGGER.log(FINER_INFO_LEVEL, message);
+	}
+
+	/**
+	 * 중대한 장해 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void severe(String message, Exception exception) {
+		if (exception == null) {
+			SEVERE_LOGGER.log(SEVERE_INFO_LEVEL, message);
+		} else {
+			SEVERE_LOGGER.log(SEVERE_INFO_LEVEL, message, exception);
+		}
+	}
+
+	/**
+	 * 중대한 장해 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void sever(String message) {
+		SEVERE_LOGGER.log(SEVERE_INFO_LEVEL, message);
+	}
+
+	/**
+	 * 잠재적인 문제 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void warning(String message, Exception exception) {
+		if (exception == null) {
+			WARNING_LOGGER.log(WARNING_INFO_LEVEL, message);
+		} else {
+			WARNING_LOGGER.log(WARNING_INFO_LEVEL, message, exception);
+		}
+	}
+
+	/**
+	 * 잠재적인 문제 메세지 정보를 기록하는 경우 사용.
+	 */
+	public static void warning(String message) {
+		WARNING_LOGGER.log(WARNING_INFO_LEVEL, message);
+	}
 }
