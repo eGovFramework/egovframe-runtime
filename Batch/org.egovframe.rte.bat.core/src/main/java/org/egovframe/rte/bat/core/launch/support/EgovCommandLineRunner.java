33dd7b6d2ac363efa7574dd654340eb5f6b86c87
KIM-HWI <79562205+kim-hwi@users.noreply.github.com>
2023-08-31 18:28:50 +0900

    LogMessage 오탈자 수정

--- Batch/org.egovframe.rte.bat.core/src/main/java/org/egovframe/rte/bat/core/launch/support/EgovCommandLineRunner.java
+++ Batch/org.egovframe.rte.bat.core/src/main/java/org/egovframe/rte/bat/core/launch/support/EgovCommandLineRunner.java
@@ -266,7 +266,7 @@ public class EgovCommandLineRunner {
 		} catch (Throwable e) {
 			String message = "Job Terminated in error: " + e.getMessage();
 			//2017.02.15 장동한 시큐어코딩(ES)-부적절한 예외 처리[CWE-253, CWE-440, CWE-754]
-			LOGGER.error("["+e.getClass()+"] Try/Catch...job tart Runing : " + e.getMessage());
+			LOGGER.error("["+e.getClass()+"] Try/Catch...job start Running : " + e.getMessage());
 			EgovCommandLineRunner.message = message;
 			return exitCodeMapper.intValue(ExitStatus.FAILED.getExitCode());
 		} finally {
