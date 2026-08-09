881aee114d8eef011d0907c28a14cb5854b60790
wantaek <wantaekchoi@gmail.com>
2026-08-09 21:44:38 +0900

    test: SimpleMessage 다중 entry 중 하나만 null인 케이스 커버리지 추가

--- Integration/org.egovframe.rte.itl.integration/src/test/java/org/egovframe/rte/itl/integration/message/simple/SimpleMessageTest.java
+++ Integration/org.egovframe.rte.itl.integration/src/test/java/org/egovframe/rte/itl/integration/message/simple/SimpleMessageTest.java
@@ -11,7 +11,7 @@ public class SimpleMessageTest {
 
     @Test
     public void testSetBodyRejectsNullValue() {
-        // EgovIntegrationMessage#setBody의 javadoc: body의 value 값 중 null이 있으면 IllegalArgumentException.
+        // SimpleMessage 생성자 javadoc: body의 value 값 중 null이 있으면 IllegalArgumentException.
         SimpleMessage message = new SimpleMessage();
         Map<String, Object> body = new HashMap<String, Object>();
         body.put("key", null);
@@ -35,6 +35,17 @@ public class SimpleMessageTest {
         SimpleMessage message = new SimpleMessage();
 
         assertThrows(IllegalArgumentException.class, () -> message.putAttachment("key", null));
+    }
+
+    @Test
+    public void testSetBodyRejectsNullValueAmongMultipleEntries() {
+        // 여러 entry 중 뒤쪽 하나만 value가 null이어도 검증을 통과해선 안 된다.
+        SimpleMessage message = new SimpleMessage();
+        Map<String, Object> body = new HashMap<String, Object>();
+        body.put("first", "ok");
+        body.put("second", null);
+
+        assertThrows(IllegalArgumentException.class, () -> message.setBody(body));
     }
 
 }
